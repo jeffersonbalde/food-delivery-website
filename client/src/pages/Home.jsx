434c20d6af78a791df8ece4d7642f9ae9586e829
Jefferson Balde <jeffersonbalde8@gmail.com>
2023-04-13 18:10:32 +0800

    setting up home section

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,29 @@
 
 function Home() {
   return (
-    <div>Home</div>
+    <div className="p-2 md:p-4">
+      <div className="md:flex">
+
+        <div className="md:w-1/2">
+          <div className="flex gap-3 bg-slate-300 w-36 px-2 items-center rounded-full ">
+            <p className="text-sm font-medium">Bike Delivery</p>
+            <img 
+              src="https://p7.hiclipart.com/preview/473/746/547/bicycle-computer-icons-cycling-bikes.jpg" 
+              alt="bike delivery" 
+              className="h-7"
+            />
+          </div>
+          <h2 className="text-4xl font-bold">The Fasted Delivery in <span>Your Home</span></h2>
+        </div>
+
+        <div className="md:w-1/2">
+          <div>Image1</div>
+          <div>Image2</div>
+          <div>Image3</div>
+        </div>
+
+      </div>
+    </div>
   )
 }
 
