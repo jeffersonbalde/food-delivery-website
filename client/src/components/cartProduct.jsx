21a1127cf274c88418c81d8c94e5e21307985245
Jefferson Balde <jeffersonbalde8@gmail.com>
2023-04-15 16:44:09 +0800

    adding functionality in deleting cart item

--- client/src/components/cartProduct.jsx
+++ client/src/components/cartProduct.jsx
@@ -1,7 +1,11 @@
 import { TbPlus, TbMinus } from "react-icons/tb"
 import { AiFillDelete } from "react-icons/ai";
+import { deleteCartItem, increaseQty, decreaseQty } from "../redux/productSlice";
+import { useDispatch } from "react-redux";
 
 function CartProduct({id, name, image, category, qty, total, price}) {
+    const dispatch = useDispatch()
+
     return (
         <div className="bg-slate-200 p-2 flex gap-4 rounded border border-slate-300">
             <div className='bg-white p-3 rounded overflow-hidden'>
@@ -11,7 +15,7 @@ function CartProduct({id, name, image, category, qty, total, price}) {
             <div className="flex flex-col gap-1 w-full">
                 <div className="flex justify-between">
                     <h3 className="font-semibold text-slate-600 capitalize text-lg md:text-xl">{name}</h3>
-                    <div className="cursor-pointer text-slate-600 hover:text-slate-700">
+                    <div className="cursor-pointer text-slate-600 hover:text-slate-700" onClick={() => dispatch(deleteCartItem(id))}>
                         <AiFillDelete />
                     </div>
                 </div>
@@ -24,9 +28,17 @@ function CartProduct({id, name, image, category, qty, total, price}) {
                 <div className=" flex justify-between">
 
                     <div className='flex gap-3 items-center'>
-                        <button className='bg-slate-500 py-1 mt-2 rounded text-white hover:bg-slate-600 p-1'><TbPlus /></button>
+                        <button 
+                            className='bg-slate-500 py-1 mt-2 rounded text-white hover:bg-slate-600 p-1' 
+                            onClick={() => dispatch(increaseQty(id))}><TbPlus 
+                        />
+                        </button>
                         <p className="font-semibold p-1">{qty}</p>
-                        <button className='bg-slate-500 py-1 mt-2 rounded text-white hover:bg-slate-600 p-1' onClick={""}><TbMinus /></button>
+                        <button 
+                            className='bg-slate-500 py-1 mt-2 rounded text-white hover:bg-slate-600 p-1' 
+                            onClick={() => dispatch(decreaseQty(id))}><TbMinus 
+                        />
+                        </button>
                     </div>
 
                     <div className="flex items-center gap-2 font-bold text-slate-600">
--- client/src/redux/productSlice.jsx
+++ client/src/redux/productSlice.jsx
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { toast } from "react-hot-toast";
 
 const initialState = {
     productList: [],
@@ -13,16 +14,36 @@ export const productSlice = createSlice({
             state.productList = [...action.payload]
         },
         addCartItem : (state, action) => {
-                    console.log(action)
-                    const total = action.payload.price
-                    state.cartItem = [...state.cartItem, {...action.payload,qty : 1, total : total}]
+                    const check = state.cartItem.some(el => el._id === action.payload._id)
+
+                    if(check) {
+                        toast("Item already in cart")
+                    }else {
+                        toast("Item add successfully")
+                        const total = action.payload.price
+                        state.cartItem = [...state.cartItem, {...action.payload,qty : 1, total : total}]
+                    }
         },
         deleteCartItem: (state, action) => {
-
+                    toast("One item delete")
+                    const index = state.cartItem.findIndex((el) => el._id === action.payload)
+                    state.cartItem.splice(index,1)
+        },
+        increaseQty : (state,action) => {
+                    const index = state.cartItem.findIndex((el) => el._id === action.payload)
+                    let qty = state.cartItem[index].qty
+                    state.cartItem[index].qty = ++qty
+        },
+        decreaseQty : (state, action) => {
+                    const index = state.cartItem.findIndex((el) => el._id === action.payload)
+                    let qty = state.cartItem[index].qty
+                    if(qty >  1) {
+                        state.cartItem[index].qty = --qty
+                    }
         }
     }
 })
 
-export const { setDataProduct, addCartItem, deleteCartItem} = productSlice.actions
+export const { setDataProduct, addCartItem, deleteCartItem, increaseQty, decreaseQty} = productSlice.actions
 
 export default productSlice.reducer
