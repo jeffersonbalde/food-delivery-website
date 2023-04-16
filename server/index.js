cbf2b4b66a3bfd8e1e52f81e128991f1114aae30
Jefferson Balde <jeffersonbalde8@gmail.com>
2023-04-16 15:26:54 +0800

    adding .env client side

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(cors());
 app.use(express.json({limit: "10mb"}));
 
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 4000
 
 // MongoDB
 console.log(process.env.MONGODB_URL)
