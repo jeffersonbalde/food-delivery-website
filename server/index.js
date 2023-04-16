2c30cb961bb44fbb65c8d8a97fbd6e659ceefda0
Jefferson Balde <jeffersonbalde8@gmail.com>
2023-04-16 13:42:37 +0800

    ff

--- server/index.js
+++ server/index.js
@@ -11,6 +11,7 @@ app.use(function(req, res, next) {
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
   });
+  
 app.use(cors());
 
 app.use(express.json({limit: "10mb"}));
