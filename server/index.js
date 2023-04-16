70ad17a898166894b954b10681976aa72ac7d5a0
Jefferson Balde <jeffersonbalde8@gmail.com>
2023-04-16 14:04:07 +0800

    dert

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const dotenv = require("dotenv").config();
 const app = express();
 
 app.use(function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', 'https://mern-food-ordering-app-gppl.vercel.app');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
