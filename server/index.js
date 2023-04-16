c1382050bede8d4840254b92e24ed3fd75cb5e39
Jefferson Balde <jeffersonbalde8@gmail.com>
2023-04-16 13:38:55 +0800

    gt

--- server/index.js
+++ server/index.js
@@ -4,7 +4,15 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv").config();
 
 const app = express();
+
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+  });
 app.use(cors());
+
 app.use(express.json({limit: "10mb"}));
 
 const PORT = process.env.PORT || 8080
