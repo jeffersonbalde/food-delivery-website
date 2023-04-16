3b40a40f0929af38e622f107269ecd5719b604e1
Jefferson Balde <jeffersonbalde8@gmail.com>
2023-04-16 09:20:22 +0800

    pushing to vercel

--- server/index.js
+++ server/index.js
@@ -1,10 +1,10 @@
 const express = require("express");
-// const cors = require("cors");
+const cors = require("cors");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv").config();
 
 const app = express();
-// app.use(cors());
+app.use(cors());
 app.use(express.json({limit: "10mb"}));
 
 const PORT = process.env.PORT || 8080
