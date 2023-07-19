2a0e585c29450ee3951b4720a5e1fec15158e85d
jeffersonbalde <jeffersonbalde8@gmail.com>
2023-07-19 18:47:35 +0800

    updating footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <div className="p-4 bg-slate-200 text-center text-slate-900 font-semibold">
       <h2>
-        Copyright © 2023 <a  href="https://www.linkedin.com/in/jeffersonbalde/"
+        Copyright © 2023 <a  href="https://github.com/jeffersonbalde"
           className="hover:text-slate-800 hover:underline hover:underline-offset-8">Jefferson Balde.</a> All rights reserved.
       </h2>
     </div>
