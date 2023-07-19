083ea5e16f45c99151e4f8ed93d3b2eaa7b9dc74
jeffersonbalde <jeffersonbalde8@gmail.com>
2023-07-19 18:46:56 +0800

    updating footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -2,13 +2,8 @@ const Footer = () => {
   return (
     <div className="p-4 bg-slate-200 text-center text-slate-900 font-semibold">
       <h2>
-        Made by{" "}
-        <a
-          href="https://www.linkedin.com/in/jeffersonbalde/"
-          className="hover:text-slate-800 hover:underline hover:underline-offset-8"
-        >
-          Jefferson Balde
-        </a>
+        Copyright © 2023 <a  href="https://www.linkedin.com/in/jeffersonbalde/"
+          className="hover:text-slate-800 hover:underline hover:underline-offset-8">Jefferson Balde.</a> All rights reserved.
       </h2>
     </div>
   );
